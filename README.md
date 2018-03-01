9138411b68df02a584887d50a5319666c63df410
Thomas J Bradley <hey@thomasjbradley.ca>
2018-03-01 11:21:43 -0500

    Allow groups of radio buttons to be validated properly

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ Also the `data-state="invalid"` attribute will be added to the `<form>` tag when
 <form novalidate class="is-validated" data-state="invalid">
 ```
 
-If there is no `<form>` tag, only form field tags, the `data-state="invalid"` attribute & `.is-validated` class will be added directly to the `<input>` itself. So, the CSS would change a little, like this:
+If there is no `<form>` tag, only field tags, the `data-state="invalid"` attribute & `.is-validated` class will be added directly to the `<input>` itself. So, the CSS would change a little, like this:
 
 ```css
 .is-validated input:invalid,
@@ -47,7 +47,7 @@ input.is-validated:invalid {
 
 It’s pretty common to have a group of checkboxes and want users to select at least one of them. HTML cannot do this by itself.
 
-To get basic checkbox group validation add the class `one-of-many` to the surrounding fieldset:
+To get basic checkbox group validation add the class `one-of-many` to the surrounding `<fieldset>`:
 
 ```html
 <fieldset class="one-of-many">
@@ -63,6 +63,8 @@ When a checkbox hasn’t been selected the `<fieldset>` will receive a `data-sta
 ```
 
 We can use the `.one-of-many[data-state="invalid"]` selector to show the appropriate error message to our users.
+
+Similarly, when there isn’t a `<form>` this will also apply to groups of radio buttons inside a `<fieldset>`.
 
 ---
 
--- index.js
+++ index.js
@@ -8,14 +8,38 @@
 
   var allForms = document.getElementsByTagName('form');
 
-  var toggleElemValidity = function (elem) {
+  var findParent = function (elem, sel) {
+    if (elem.parentNode.matches('body')) return false;
+    if (elem.parentNode.matches(sel)) return elem.parentNode;
+
+    return findParent(elem.parentNode, sel);
+  };
+
+  var toggleElemValidity = function (elem, isValid) {
+    if (!elem) return;
+
     elem.classList.add('is-validated');
 
-    if (!elem.checkValidity()) {
-      elem.dataset.state = 'invalid';
+    if (typeof isValid !== 'undefined') {
+      elem.dataset.state = (isValid === false) ? 'invalid' : 'valid';
     } else {
-      elem.dataset.state = 'valid';
+      if (elem.checkValidity() === false) {
+        elem.dataset.state = 'invalid';
+      } else {
+        elem.dataset.state = 'valid';
+      }
     }
+  };
+
+  var isOneOfManyValid = function (elem) {
+    var isOneSelected = false;
+    var allChecks = elem.querySelectorAll('[type="checkbox"], [type="radio"]');
+
+    [].forEach.call(allChecks, function (input) {
+      if (input.checked) isOneSelected = true;
+    });
+
+    return isOneSelected;
   };
 
   var validateForm = function (e) {
@@ -29,17 +53,11 @@
     if (!oneOfManyElems) return;
 
     [].forEach.call(oneOfManyElems, function (checkGroup) {
-      var isOneSelected = false;
-      var allChecks = checkGroup.querySelectorAll('[type="checkbox"]');
-
-      [].forEach.call(allChecks, function (checkbox) {
-        if (checkbox.checked) isOneSelected = true;
-      });
-
-      if (!isOneSelected) {
+      if (!isOneOfManyValid(checkGroup)) {
         e.preventDefault();
-        toggleElemValidity(e.target);
-        checkGroup.dataset.state = 'invalid';
+        toggleElemValidity(checkGroup, false);
+      } else {
+        toggleElemValidity(checkGroup, true);
       }
     });
   };
@@ -50,10 +68,24 @@
 
   if (allForms.length <= 0) {
     document.body.addEventListener('keyup', function (e) {
+      var parentFieldset;
+
       if (e.keyCode != 13) return;
 
       if (['input', 'select'].indexOf(e.target.tagName.toLowerCase()) > -1) {
         toggleElemValidity(e.target);
+
+        if(e.target.getAttribute('type') === 'radio') {
+          toggleElemValidity(findParent(e.target, 'fieldset'), e.target.checkValidity());
+        }
+
+        if(e.target.getAttribute('type') === 'checkbox') {
+          parentFieldset = findParent(e.target, 'fieldset');
+
+          if (parentFieldset.classList.contains('one-of-many')) {
+            toggleElemValidity(parentFieldset, isOneOfManyValid(parentFieldset));
+          }
+        }
       }
 
       if (['textarea'].indexOf(e.target.tagName.toLowerCase()) > -1) {
