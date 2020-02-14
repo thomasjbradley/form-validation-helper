2d0a8b968a8728d4b4e64516a069d5046512c78c
Thomas J Bradley <hey@thomasjbradley.ca>
2020-02-14 16:31:53 -0500

    Move to modern JS & fix a bug with select boxes

--- index.js
+++ index.js
@@ -3,56 +3,64 @@
  * @copyright © 2018 Thomas J Bradley
  * @license MIT
  */
-(function () {
-  'use strict';
+(function() {
+  "use strict";
 
-  var allForms = document.getElementsByTagName('form');
+  const allForms = document.getElementsByTagName("form");
 
-  var findParent = function (elem, sel) {
-    if (elem.parentNode.matches('body')) return false;
+  const findParent = (elem, sel) => {
+    if (elem.parentNode.matches("body")) return false;
     if (elem.parentNode.matches(sel)) return elem.parentNode;
 
     return findParent(elem.parentNode, sel);
   };
 
-  var toggleElemValidity = function (elem, isValid) {
+  const toggleElemValidity = (elem, isValid) => {
     if (!elem) return;
 
-    elem.classList.add('is-validated');
+    elem.classList.add("is-validated");
 
-    if (typeof isValid !== 'undefined') {
-      elem.dataset.state = (isValid === false) ? 'invalid' : 'valid';
+    if (typeof isValid !== "undefined") {
+      elem.dataset.state = isValid === false ? "invalid" : "valid";
     } else {
       if (elem.checkValidity() === false) {
-        elem.dataset.state = 'invalid';
+        elem.dataset.state = "invalid";
       } else {
-        elem.dataset.state = 'valid';
+        elem.dataset.state = "valid";
       }
     }
   };
 
-  var isOneOfManyValid = function (elem) {
-    var isOneSelected = false;
-    var allChecks = elem.querySelectorAll('[type="checkbox"], [type="radio"]');
+  const isOneOfManyValid = elem => {
+    const allChecks = elem.querySelectorAll(
+      '[type="checkbox"], [type="radio"]'
+    );
+    let isOneSelected = false;
 
-    [].forEach.call(allChecks, function (input) {
+    [].forEach.call(allChecks, input => {
       if (input.checked) isOneSelected = true;
     });
 
     return isOneSelected;
   };
 
-  var validateForm = function (e) {
-    var oneOfManyElems = this.querySelectorAll('fieldset.one-of-many');
+  const validateForm = e => {
+    const oneOfManyElems = e.target.querySelectorAll("fieldset.one-of-many");
 
-    if (!this.checkValidity()) {
+    if (!e.target.checkValidity()) {
       e.preventDefault();
       toggleElemValidity(e.target);
+      [].forEach.call(
+        e.target.querySelectorAll("input, select, textarea"),
+        ev => {
+          toggleElemValidity(ev);
+        }
+      );
     }
 
     if (!oneOfManyElems) return;
 
-    [].forEach.call(oneOfManyElems, function (checkGroup) {
+    [].forEach.call(oneOfManyElems, checkGroup => {
       if (!isOneOfManyValid(checkGroup)) {
         e.preventDefault();
         toggleElemValidity(checkGroup, false);
@@ -62,35 +70,41 @@
     });
   };
 
-  [].forEach.call(allForms, function (form) {
-    form.addEventListener('submit', validateForm);
+  [].forEach.call(allForms, form => {
+    form.addEventListener("submit", validateForm);
   });
 
   if (allForms.length <= 0) {
-    document.body.addEventListener('keyup', function (e) {
-      var parentFieldset;
+    document.body.addEventListener("keyup", e => {
+      let parentFieldset;
 
       if (e.keyCode != 13) return;
 
-      if (['input', 'select'].indexOf(e.target.tagName.toLowerCase()) > -1) {
+      if (["input", "select"].indexOf(e.target.tagName.toLowerCase()) > -1) {
         toggleElemValidity(e.target);
 
-        if(e.target.getAttribute('type') === 'radio') {
-          toggleElemValidity(findParent(e.target, 'fieldset'), e.target.checkValidity());
+        if (e.target.getAttribute("type") === "radio") {
+          toggleElemValidity(
+            findParent(e.target, "fieldset"),
+            e.target.checkValidity()
+          );
         }
 
-        if(e.target.getAttribute('type') === 'checkbox') {
-          parentFieldset = findParent(e.target, 'fieldset');
+        if (e.target.getAttribute("type") === "checkbox") {
+          parentFieldset = findParent(e.target, "fieldset");
 
-          if (parentFieldset.classList.contains('one-of-many')) {
-            toggleElemValidity(parentFieldset, isOneOfManyValid(parentFieldset));
+          if (parentFieldset.classList.contains("one-of-many")) {
+            toggleElemValidity(
+              parentFieldset,
+              isOneOfManyValid(parentFieldset)
+            );
           }
         }
       }
 
-      if (['textarea'].indexOf(e.target.tagName.toLowerCase()) > -1) {
+      if (["textarea"].indexOf(e.target.tagName.toLowerCase()) > -1) {
         if (e.altKey || e.ctrlKey) toggleElemValidity(e.target);
       }
     });
   }
-}());
+})();
